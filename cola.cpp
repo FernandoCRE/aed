f0adb277675ec07f9b718320aca1770b76c41cb8
fernandocre <fernando_hu@outlook.com>
2026-04-09 22:07:18 -0500

    Update cola.cpp

--- cola.cpp
+++ cola.cpp
@@ -15,6 +15,7 @@ struct cola {
     bool lleno();
     bool push(T valor);
     bool pop(T& valor);
+    void print();
 };
 
 template <class T, int N>
@@ -24,6 +25,36 @@ cola<T, N>::cola() {
     head = nullptr;
     tail = nullptr;
 }
+
+template <class T, int N>
+void cola<T, N>::print() {
+    if (vacio()) {
+        cout << "La cola esta vacia.\n";
+        return;
+    }
+
+    // Usamos un puntero temporal para no mover el 'head' original
+    T* t = head;
+
+    while (true) {
+        cout << *t << " "; // Imprimimos el valor donde apunta t
+
+        // Lógica para que el puntero 't' se mueva circularmente
+        if (t == arr + nro_elementos - 1) 
+            t = arr; // Si llega al final del arreglo, salta al inicio
+        else 
+            t++;     // Si no, avanza a la siguiente posición
+
+        // Si después de avanzar, t alcanzó al tail, ya terminamos
+        if (t == tail) break;
+    }
+    cout << "\n";
+}
+
+
+
+
+
 
 template <class T, int N>
 cola<T, N>::~cola() {
@@ -74,44 +105,36 @@ bool cola<T, N>::pop(T& valor) {
 
     valor = *head;
 
-    // si solo hay un elemento
-    if ((head == tail - 1) || (head == arr + nro_elementos - 1 && tail == arr)) {
+    // 1. Calculamos cuál sería la posición SIGUIENTE del head
+    T* proximo_head;
+    if (head == arr + nro_elementos - 1) 
+        proximo_head = arr;
+    else 
+        proximo_head = head + 1;
+
+    // 2. Si el siguiente lugar del head es donde está el tail, 
+    // significa que acabamos de sacar el último elemento.
+    if (proximo_head == tail) {
         head = nullptr;
         tail = nullptr;
-        return true;
+    } else {
+        head = proximo_head;
     }
-
-    // pila circular
-    if (head == arr + nro_elementos - 1)
-        head = arr;
-    else
-        head++;
 
     return true;
 }
 
 int main() {
     cola<int> c;
-
-    c.push(1);
-    c.push(2);
-    c.push(3);
-    c.push(4);
-
-    int x;
-
-    c.pop(x); cout << "es: " << x << endl;
-    c.pop(x); cout << "es: " << x << endl;
-
-    c.push(13);
-    c.push(20);
-
-    c.pop(x); cout << "es: " << x << endl;
-    c.pop(x); cout << "es: " << x << endl;
-    c.pop(x); cout << "es: " << x << endl;
-    c.pop(x); cout << "es: " << x << endl;
-
     
+    
+    c.push(1);
+    c.print();
+    int x;
+    c.pop(x);
+    c.print();
+    c.pop(x);
+    c.print();
     
     return 0;
 }
